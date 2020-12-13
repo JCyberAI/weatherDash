f769d72c584691ae5168e49fa3f14f0e2f0e32a9
ArnoldIngrave <jalyn.campbell@yahoo.com>
2020-12-13 11:19:09 -0500

    commit on main

--- script.js
+++ script.js
@@ -10,12 +10,16 @@ var search = document.getElementById("search")
 function shuffle {
     for (i = 0; i < capitalStates.length; i++) {
         capitalStates[i];
-    }
-}
+    };
 
-if (search.clicked == true) {
+    if (search.clicked == true) {
     capitalStates.stringify (Call API for capitalStates Object data to be filled?);
-} else if (clear.clicked == true) {
-    NAVBAR.clearText?;
-}
+    } else if (clear.clicked == true) {
+         NAVBAR.clearText?;
+    } else {
+        console.log(search);
+    };
+};
+
+
 */
