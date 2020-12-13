0f375ae0884c8d1de1807baa35579bdb12930180
ArnoldIngrave <jalyn.campbell@yahoo.com>
2020-12-13 11:07:56 -0500

    commit on main

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-/* var capitalStates = [
+/* const capitalStates = [
     state: "";
     captial: "";
     currentTemp: "";
     weeklyTemp: "";
 ]
 
+var search = document.getElementById("search")
+
+function shuffle {
+    for (i = 0; i < capitalStates.length; i++) {
+        capitalStates[i];
+    }
+}
+
+if (search.clicked == true) {
+    text.stringify
+}
 */
